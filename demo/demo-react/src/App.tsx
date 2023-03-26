26550ee6be860da9b6465fa29c8b43f4530c0cba
YMSpektor <spektor.ym@gmail.com>
2023-03-26 13:23:36 +0300

    Update demo layout

--- demo/demo-react/src/App.tsx
+++ demo/demo-react/src/App.tsx
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import React, { useState } from 'react';
 import DiagramToolbar from './components/DiagramToolbar';
 import DiagramViewer from './components/DiagramViewer';
@@ -7,14 +8,15 @@ function App() {
     const [scale, setScale] = useState(100);
 
     return (
-        <>
+        <div css={{display: "flex", flexDirection: "column", height: "100vh"}}>
             <DiagramToolbar scale={scale} onScaleChange={setScale} />
-            <DiagramViewer 
+            <DiagramViewer
+                css={{flex: 1}}
                 diagram={doc} 
                 diagramSize={{width: 2100, height: 2970}} 
                 scale={scale} unit="mm" unitMultiplier={0.1} 
             />
-        </>
+        </div>
     )
 }
 
--- demo/demo-react/src/components/DiagramViewer.tsx
+++ demo/demo-react/src/components/DiagramViewer.tsx
@@ -1,17 +1,6 @@
 /** @jsxImportSource @emotion/react */
-import { useLayoutEffect, useRef } from "react";
+import { HTMLAttributes, useLayoutEffect, useRef } from "react";
 import { Diagram } from "carnelian-diagram";
-
-const containerStyles = {
-    overflow: "auto",
-    display: "flex"
-};
-
-const svgStyles = {
-    margin: "auto",
-    flex: "0 0 auto",
-    overflow: "hidden"
-}
 
 interface DiagramViewerProps {
     diagram: Diagram;
@@ -21,30 +10,31 @@ interface DiagramViewerProps {
     unitMultiplier?: number;
 }
 
-function DiagramViewer(props: DiagramViewerProps) {
+function DiagramViewer(props: DiagramViewerProps & HTMLAttributes<HTMLDivElement>) {
+    let {diagram, diagramSize, scale, unit, unitMultiplier, ...divProps} = props;
     const container = useRef<HTMLDivElement>(null);
     const root = useRef<SVGSVGElement>(null);
 
-    const unit = props.unit || "px";
-    const unitMultiplier = props.unitMultiplier || 1;
-    const width = `${props.diagramSize.width * (props.scale / 100) * unitMultiplier}${unit}`;
-    const height = `${props.diagramSize.height * (props.scale / 100) * unitMultiplier}${unit}`;
+    unit = unit || "px";
+    unitMultiplier = unitMultiplier || 1;
+    const width = `${diagramSize.width * (scale / 100) * unitMultiplier}${unit}`;
+    const height = `${diagramSize.height * (scale / 100) * unitMultiplier}${unit}`;
 
     useLayoutEffect(() => {
-        root.current && props.diagram.attach(root.current);
+        root.current && diagram.attach(root.current);
 
         return () => {
-            props.diagram.detach();
+            diagram.detach();
         }
-    }, [props.diagram]);
+    }, [diagram]);
 
     return (
-        <div ref={container} css={containerStyles}>
-                <svg xmlns="http://www.w3.org/2000/svg"
-                    viewBox={[0, 0, props.diagramSize.width, props.diagramSize.height].join(' ')} 
-                    style={{width, height}} css={svgStyles}
-                    ref={root}>
-                </svg>
+        <div ref={container} css={{display: "flex", overflow: "auto"}} {...divProps}>
+            <svg xmlns="http://www.w3.org/2000/svg"
+                viewBox={[0, 0, diagramSize.width, diagramSize.height].join(' ')}
+                style={{width, height}} css={{flex: "1 0 auto"}}
+                ref={root}>
+            </svg>
         </div>
     );
 }
