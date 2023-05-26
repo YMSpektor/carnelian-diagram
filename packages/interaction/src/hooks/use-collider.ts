9f9947a1df0bd707a7a572837e89f352e28edcc2
YMSpektor <spektor.ym@gmail.com>
2023-05-26 15:28:27 +0300

    Fix tolerance calculations for useCollider

--- packages/interaction/src/hooks/use-collider.ts
+++ packages/interaction/src/hooks/use-collider.ts
@@ -3,14 +3,17 @@ import { collide, Collider, PointCollider } from "../collisions/colliders";
 import { HitArea, HitTestCallback } from "../hit-tests";
 import { useHitTest, useIntersectionTest } from ".";
 import { IntersectionTestCallback } from "../intersection-tests";
-import { inflateRect, pointInRect } from "../geometry";
+import { distance, inflateRect, pointInRect, transformPoint } from "../geometry";
 
 export function useCollider<T>(collider: Collider<T>, hitArea: HitArea, priority: number = 0, 
     hitTestTolerance: number = 0, element?: DiagramElementNode
 ) {
     const hitTestCallback: HitTestCallback = (point, transform) => {
         const elemPoint = point.matrixTransform(transform);
-        const tolerance = hitTestTolerance * transform.a;
+        const v1 = transformPoint({ x: 0, y: 0 }, transform);
+        const v2 = transformPoint({ x: 1, y: 0 }, transform);
+        const scale = distance(v1, v2);
+        const tolerance = hitTestTolerance * scale;
         const bounds = collider.bounds ? inflateRect(collider.bounds, tolerance) : null;
         return (!bounds || pointInRect(elemPoint, bounds)) && !!collide(PointCollider(elemPoint), collider, tolerance);
     }
