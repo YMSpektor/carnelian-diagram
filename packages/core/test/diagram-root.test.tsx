b7b84fd8dbdc7ea04a142c5f8e469019d7160a57
YMSpektor <spektor.ym@gmail.com>
2023-06-02 21:12:03 +0300

    Add new core tests

--- packages/core/test/diagram-root.test.tsx
+++ packages/core/test/diagram-root.test.tsx
@@ -25,35 +25,56 @@ describe("Diagram root tests", () => {
         root = DiagramDOM.createRoot(diagram, svg, DiagramRoot);
     });
 
-    test("Initial isValid should be false", () => {
-        expect(root.isValid()).toEqual(false);
+    describe("isValid", () => {
+        test("Initial isValid should be false", () => {
+            expect(root.isValid()).toEqual(false);
+        });
+    
+        test("isValid should be true after rendering", () => {
+            root.render();
+    
+            expect(root.isValid()).toEqual(true);
+        });
+    
+        test("isValid should be false after adding a new element", () => {
+            root.render();
+            expect(root.isValid()).toEqual(true);
+    
+            diagram.add(TestElement, { cx: 20, cy: 20, r: 10 });
+            expect(root.isValid()).toEqual(false);
+        });
+    
+        test("isValid should be false after updating the element", () => {
+            const element = diagram.add(TestElement, { cx: 20, cy: 20, r: 10 });
+            root.render();
+            expect(root.isValid()).toEqual(true);
+    
+            diagram.update(element, {...element.props});
+            expect(root.isValid()).toEqual(false);
+        });
     });
 
-    test("isValid should be true after rendering", () => {
-        root.render();
-
-        expect(root.isValid()).toEqual(true);
-    });
-
-    test("Added element is found in the DOM", () => {
-        diagram.add(TestElement, { cx: 20, cy: 20, r: 10 });
-        root.render();
-
-        const element = document.getElementById("test_element");
-
-        expect(element).toBeTruthy();
-    });
-
-    test("Added element has correct tagName and attributes", () => {
-        const props = { cx: 20, cy: 20, r: 10 };
-        diagram.add(TestElement, props);
-        root.render();
-
-        const element = document.getElementById("test_element");
-
-        expect(element?.tagName).toEqual("circle");
-        expect(element?.getAttribute("cx")).toEqual(props.cx.toString());
-        expect(element?.getAttribute("cy")).toEqual(props.cy.toString());
-        expect(element?.getAttribute("r")).toEqual(props.r.toString());
+    describe("Add new element", () => {
+        test("Added element should be found in the DOM", () => {
+            diagram.add(TestElement, { cx: 20, cy: 20, r: 10 });
+            root.render();
+    
+            const element = document.getElementById("test_element");
+    
+            expect(element).toBeTruthy();
+        });
+    
+        test("Added element should have correct tagName and attributes", () => {
+            const props = { cx: 20, cy: 20, r: 10 };
+            diagram.add(TestElement, props);
+            root.render();
+    
+            const element = document.getElementById("test_element");
+    
+            expect(element?.tagName).toEqual("circle");
+            expect(element?.getAttribute("cx")).toEqual(props.cx.toString());
+            expect(element?.getAttribute("cy")).toEqual(props.cy.toString());
+            expect(element?.getAttribute("r")).toEqual(props.r.toString());
+        });
     });
 });
