27270bb4f52fc9d7cd2dd3a39c52174e6e98bec5
YMSpektor <spektor.ym@gmail.com>
2023-06-08 16:11:11 +0300

    Add selection tests

--- packages/interaction/test/interaction-controller.test.tsx
+++ packages/interaction/test/interaction-controller.test.tsx
@@ -33,19 +33,212 @@ describe("InteactionController tests", () => {
         controller.attach(svg);
     });
 
-    test("Element must be selected after clicking", () => {
-        const element = diagram.add(TestElement, { x: 0, y: 0, width: 100, height: 50 });
-        root.render();
+    describe("Selection tests", () => {
+        test("Element must be selected after clicking", () => {
+            const element = diagram.add(TestElement, { x: 0, y: 0, width: 100, height: 50 });
+            root.render();
 
-        let clientPos = controller.diagramToClient(new DOMPoint(0, 0));
-        let event = new window.PointerEvent("pointerdown", {
-            button: 0,
-            clientX: clientPos.x,
-            clientY: clientPos.y
+            expect(controller.isSelected(element)).toEqual(false);
+    
+            let clientPos = controller.diagramToClient(new DOMPoint(0, 0));
+            let event = new PointerEvent("pointerdown", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            root.render();
+    
+            expect(controller.isSelected(element)).toEqual(true);
         });
-        svg.dispatchEvent(event);
-        root.render();
 
-        expect(controller.isSelected(element)).toEqual(true);
+        test("Element must be deselected after clicking outside", () => {
+            const element = diagram.add(TestElement, { x: 0, y: 0, width: 100, height: 50 });
+            root.render();
+
+            expect(controller.isSelected(element)).toEqual(false);
+    
+            let clientPos = controller.diagramToClient(new DOMPoint(0, 0));
+            let event = new PointerEvent("pointerdown", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            event = new PointerEvent("pointerup", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            root.render();
+    
+            expect(controller.isSelected(element)).toEqual(true);
+
+            clientPos = controller.diagramToClient(new DOMPoint(200, 200));
+            event = new PointerEvent("pointerdown", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            root.render();
+
+            expect(controller.isSelected(element)).toEqual(false);
+        });
+
+        test("Element must not be deselected after clicking inside", () => {
+            const element = diagram.add(TestElement, { x: 0, y: 0, width: 100, height: 50 });
+            root.render();
+
+            expect(controller.isSelected(element)).toEqual(false);
+    
+            let clientPos = controller.diagramToClient(new DOMPoint(0, 0));
+            let event = new PointerEvent("pointerdown", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            event = new PointerEvent("pointerup", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            root.render();
+    
+            expect(controller.isSelected(element)).toEqual(true);
+
+            clientPos = controller.diagramToClient(new DOMPoint(0, 0));
+            event = new PointerEvent("pointerdown", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            root.render();
+
+            expect(controller.isSelected(element)).toEqual(true);
+        });
+
+        test("Selecting with Shift key must select two elements", () => {
+            const element1 = diagram.add(TestElement, { x: 0, y: 0, width: 100, height: 50 });
+            const element2 = diagram.add(TestElement, { x: 20, y: 20, width: 100, height: 50 });
+            root.render();
+
+            expect(controller.isSelected(element1)).toEqual(false);
+            expect(controller.isSelected(element2)).toEqual(false);
+    
+            let clientPos = controller.diagramToClient(new DOMPoint(0, 0));
+            let event = new PointerEvent("pointerdown", {
+                button: 0,
+                shiftKey: true,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            event = new PointerEvent("pointerup", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            root.render();
+    
+            expect(controller.isSelected(element1)).toEqual(true);
+            expect(controller.isSelected(element2)).toEqual(false);
+
+            clientPos = controller.diagramToClient(new DOMPoint(20, 20));
+            event = new PointerEvent("pointerdown", {
+                button: 0,
+                shiftKey: true,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            event = new PointerEvent("pointerup", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            root.render();
+    
+            expect(controller.isSelected(element1)).toEqual(true);
+            expect(controller.isSelected(element2)).toEqual(true);
+        });
+
+        test("Clicking with Shift key must toggle selection", () => {
+            const element = diagram.add(TestElement, { x: 0, y: 0, width: 100, height: 50 });
+            root.render();
+
+            expect(controller.isSelected(element)).toEqual(false);
+    
+            let clientPos = controller.diagramToClient(new DOMPoint(0, 0));
+            let event = new PointerEvent("pointerdown", {
+                button: 0,
+                shiftKey: true,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            event = new PointerEvent("pointerup", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            root.render();
+    
+            expect(controller.isSelected(element)).toEqual(true);
+
+            clientPos = controller.diagramToClient(new DOMPoint(0, 0));
+            event = new PointerEvent("pointerdown", {
+                button: 0,
+                shiftKey: true,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            root.render();
+
+            expect(controller.isSelected(element)).toEqual(false);
+        });
+
+        test("Selection rect must select all elements inside", () => {
+            const element1 = diagram.add(TestElement, { x: 0, y: 0, width: 100, height: 50 });
+            const element2 = diagram.add(TestElement, { x: 20, y: 20, width: 100, height: 50 });
+            root.render();
+
+            expect(controller.isSelected(element1)).toEqual(false);
+            expect(controller.isSelected(element2)).toEqual(false);
+    
+            let clientPos = controller.diagramToClient(new DOMPoint(200, 200));
+            let event = new PointerEvent("pointerdown", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            clientPos = controller.diagramToClient(new DOMPoint(0, 0));
+            event = new PointerEvent("pointermove", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            clientPos = controller.diagramToClient(new DOMPoint(0, 0));
+            event = new PointerEvent("pointerup", {
+                button: 0,
+                clientX: clientPos.x,
+                clientY: clientPos.y
+            });
+            svg.dispatchEvent(event);
+            root.render();
+    
+            expect(controller.isSelected(element1)).toEqual(true);
+            expect(controller.isSelected(element2)).toEqual(true);
+        });
     });
 });
