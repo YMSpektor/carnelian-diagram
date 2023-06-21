3852e232f5749311a5480d4ae7c9670a3fefa668
YMSpektor <spektor.ym@gmail.com>
2023-06-21 09:47:48 +0300

    Implement editing for stroke and text properties

--- examples/react-example/src/components/DiagramPropertiesPanel.tsx
+++ examples/react-example/src/components/DiagramPropertiesPanel.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { InteractionController, isGridSnappingService, isPaperService, Paper } from "@carnelian-diagram/interaction";
 import { Accordion, AccordionDetails, AccordionSummary, Box, FormControl, FormControlLabel, FormLabel, InputAdornment, InputLabel, MenuItem, Radio, RadioGroup, Select, Tab, Tabs, TextField, Typography } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
@@ -7,7 +7,7 @@ import FormGroup from "@mui/material/FormGroup";
 import Checkbox from "@mui/material/Checkbox";
 import { Diagram, DiagramElementNode, DiagramElementProps } from "@carnelian-diagram/core";
 import ColorInput from "./ColorInput";
-import { ClosedFigureStyleProps } from "@carnelian-diagram/shapes";
+import { ClosedFigureStyleProps, TextStyleProps } from "@carnelian-diagram/shapes";
 
 export interface DiagramPropertiesPanelProps {
     diagram: Diagram;
@@ -36,16 +36,16 @@ const DiagramPropertiesTab = (props: DiagramPropertiesPanelProps) => {
     const paperService = props.controller.getService(isPaperService);
     const gridSnappingService = props.controller.getService(isGridSnappingService);
 
-    const [ pageOrientation, setPageOrientation ] = useState(getPageOrientation());
-    const [ pageSize, setPageSize ] = useState(PAGE_SIZES.find(x => 
+    const [pageOrientation, setPageOrientation] = useState(getPageOrientation());
+    const [pageSize, setPageSize] = useState(PAGE_SIZES.find(x => 
         getPaperWidth(x) === paperService?.paper?.width && 
         getPaperHeight(x) === paperService?.paper?.height));
-    const [ displayGrid, setDisplayGrid ] = useState(!!paperService?.paper?.majorGridSize || !!paperService?.paper?.minorGridSize);
-    const [ majorGridSize, setMajorGridSize ] = useState<string>(((paperService?.paper?.majorGridSize || 0) * props.unitMultiplier).toString());
-    const [ minorGridSize, setMinorGridSize ] = useState<string>(((paperService?.paper?.minorGridSize || 0) * props.unitMultiplier).toString());
-    const [ snapToGrid, setSnapToGrid ] = useState(!!gridSnappingService?.snapGridSize || !!gridSnappingService?.snapAngle);
-    const [ snapGridSize, setSnapGridSize ] = useState<string>(((gridSnappingService?.snapGridSize || 0) * props.unitMultiplier).toString());
-    const [ snapAngle, setSnapAngle ] = useState<string>((gridSnappingService?.snapAngle || 0).toString());
+    const [displayGrid, setDisplayGrid] = useState(!!paperService?.paper?.majorGridSize || !!paperService?.paper?.minorGridSize);
+    const [majorGridSize, setMajorGridSize] = useState<string>(((paperService?.paper?.majorGridSize || 0) * props.unitMultiplier).toString());
+    const [minorGridSize, setMinorGridSize] = useState<string>(((paperService?.paper?.minorGridSize || 0) * props.unitMultiplier).toString());
+    const [snapToGrid, setSnapToGrid] = useState(!!gridSnappingService?.snapGridSize || !!gridSnappingService?.snapAngle);
+    const [snapGridSize, setSnapGridSize] = useState<string>(((gridSnappingService?.snapGridSize || 0) * props.unitMultiplier).toString());
+    const [snapAngle, setSnapAngle] = useState<string>((gridSnappingService?.snapAngle || 0).toString());
 
     function getPageOrientation(): string {
         return paperService?.paper && paperService.paper.width > paperService.paper.height ? "landscape" : "portrait";
@@ -95,11 +95,11 @@ const DiagramPropertiesTab = (props: DiagramPropertiesPanelProps) => {
     function updateMinorGridSize(value: string) {
         setMinorGridSize(value);
         let gridSize = parseFloat(value);
-        gridSize = isNaN(gridSize) ? 0 : gridSize;
+        gridSize = isNaN(gridSize) ? 0 : gridSize / props.unitMultiplier;
         if (paperService?.paper) {
             paperService.paper = {
                 ...paperService.paper, 
-                minorGridSize: value ? gridSize / props.unitMultiplier : undefined, 
+                minorGridSize: value ? gridSize : undefined, 
             };
             props.onPaperChange(paperService.paper);
         }
@@ -108,11 +108,11 @@ const DiagramPropertiesTab = (props: DiagramPropertiesPanelProps) => {
     function updateMajorGridSize(value: string) {
         setMajorGridSize(value);
         let gridSize = parseFloat(value);
-        gridSize = isNaN(gridSize) ? 0 : gridSize;
+        gridSize = isNaN(gridSize) ? 0 : gridSize / props.unitMultiplier;
         if (paperService?.paper) {
             paperService.paper = {
                 ...paperService.paper, 
-                majorGridSize: value ? gridSize / props.unitMultiplier : undefined, 
+                majorGridSize: value ? gridSize : undefined, 
             };
             props.onPaperChange(paperService.paper);
         }
@@ -129,9 +129,9 @@ const DiagramPropertiesTab = (props: DiagramPropertiesPanelProps) => {
     function updateSnapGridSize(value: string) {
         setSnapGridSize(value);
         let gridSize = parseFloat(value);
-        gridSize = isNaN(gridSize) ? 0 : gridSize;
+        gridSize = isNaN(gridSize) ? 0 : gridSize / props.unitMultiplier;
         if (gridSnappingService) {
-            gridSnappingService.snapGridSize = value ? gridSize / props.unitMultiplier : undefined;
+            gridSnappingService.snapGridSize = value ? gridSize : undefined;
         }
     }
 
@@ -234,19 +234,88 @@ const DiagramPropertiesTab = (props: DiagramPropertiesPanelProps) => {
     );
 }
 
+function getFillColor(selectedElements: DiagramElementNode[]) {
+    const defaultFillColor = "#ffffff";
+    return selectedElements.length ? (selectedElements[0].props as ClosedFigureStyleProps).style?.fill || defaultFillColor : "";
+}
+
+function getStrokeColor(selectedElements: DiagramElementNode[]) {
+    const defaultStrokeColor = "#000000";
+    return selectedElements.length ? (selectedElements[0].props as ClosedFigureStyleProps).style?.stroke || defaultStrokeColor : "";
+}
+
+function getStrokeWidth(selectedElements: DiagramElementNode[], unitMultiplier: number) {
+    const defaultStrokeWidth = 0.25;
+    return selectedElements.length ? (parseFloat((selectedElements[0].props as ClosedFigureStyleProps).style?.strokeWidth?.toString() || "") || 0) * unitMultiplier || defaultStrokeWidth : "";
+}
+
+function getTextColor(selectedElements: DiagramElementNode[]) {
+    const defaultFontColor = "#000000";
+    return selectedElements.length ? (selectedElements[0].props as TextStyleProps).textStyle?.fill || defaultFontColor : "";
+}
+
+function getFontSize(selectedElements: DiagramElementNode[], unitMultiplier: number) {
+    const defaultFontSize = 1;
+    return selectedElements.length ? (parseFloat((selectedElements[0].props as TextStyleProps).textStyle?.fontSize?.toString() || "") || 0) * unitMultiplier || defaultFontSize : "";
+}
+
 const ElementsPropertiesTab = (props: DiagramPropertiesPanelProps) => {
+    // eslint-disable-next-line
+    const [_, forceUpdate] = useReducer(x => x + 1, 0);
+    const [strokeWidth, setStrokeWidth] = useState(getStrokeWidth(props.selectedElements, props.unitMultiplier));
+    const [fontSize, setFontSize] = useState(getFontSize(props.selectedElements, props.unitMultiplier));
+
+    useEffect(() => {
+        const subscription = props.diagram.subscribe(() => {
+            setStrokeWidth(getStrokeWidth(props.selectedElements, props.unitMultiplier));
+            setFontSize(getFontSize(props.selectedElements, props.unitMultiplier));
+            forceUpdate();
+        });
+
+        return () => subscription.unsubscribe();
+    }, [props.diagram, props.selectedElements, props.unitMultiplier]);
+
+    useEffect(() => {
+        setStrokeWidth(getStrokeWidth(props.selectedElements, props.unitMultiplier));
+    }, [props.selectedElements, props.unitMultiplier]);
+
     function updateElement<T>(element: DiagramElementNode<T>, elementProps: DiagramElementProps<T>) {
         props.diagram.update(element, elementProps);
-    }
-
-    function getFillColor() {
-        const defaultFillColor = "#ffffff";
-        return props.selectedElements.length ? (props.selectedElements[0].props as ClosedFigureStyleProps).style?.fill || defaultFillColor : "";
     }
 
     function updateFillColor(value: string) {
         props.selectedElements.forEach(element => {
             updateElement<ClosedFigureStyleProps>(element, { ...element.props, style: { ...element.props.style, fill: value } });
+        });
+    }
+
+    function updateStrokeColor(value: string) {
+        props.selectedElements.forEach(element => {
+            updateElement<ClosedFigureStyleProps>(element, { ...element.props, style: { ...element.props.style, stroke: value } });
+        });
+    }
+
+    function updateStrokeWidth(value: string) {
+        setStrokeWidth(value);
+        let strokeWidth = parseFloat(value);
+        strokeWidth = isNaN(strokeWidth) ? 0 : strokeWidth / props.unitMultiplier;
+        props.selectedElements.forEach(element => {
+            updateElement<ClosedFigureStyleProps>(element, { ...element.props, style: { ...element.props.style, strokeWidth: strokeWidth } });
+        });
+    }
+
+    function updateTextColor(value: string) {
+        props.selectedElements.forEach(element => {
+            updateElement<TextStyleProps>(element, { ...element.props, textStyle: { ...element.props.textStyle, fill: value } });
+        });
+    }
+
+    function updateFontSize(value: string) {
+        setFontSize(value);
+        let fontSize = parseFloat(value);
+        fontSize = isNaN(fontSize) ? 0 : fontSize / props.unitMultiplier;
+        props.selectedElements.forEach(element => {
+            updateElement<TextStyleProps>(element, { ...element.props, textStyle: { ...element.props.textStyle, fontSize: fontSize } });
         });
     }
 
@@ -258,20 +327,35 @@ const ElementsPropertiesTab = (props: DiagramPropertiesPanelProps) => {
                         <Typography>Fill</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                    <ColorInput 
-                        fullWidth variant="outlined" size="small" margin="dense"
-                        label="Fill color"
-                        value={getFillColor()}
-                        onChange={updateFillColor}
-                    />
+                        <ColorInput 
+                            fullWidth variant="outlined" size="small" margin="dense"
+                            label="Fill color"
+                            value={getFillColor(props.selectedElements)}
+                            onChange={updateFillColor}
+                        />
                     </AccordionDetails>
                 </Accordion>
                 <Accordion disableGutters={true}  defaultExpanded={true}>
                     <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                        <Typography>Line</Typography>
+                        <Typography>Stroke</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-
+                        <ColorInput 
+                            fullWidth variant="outlined" size="small" margin="dense"
+                            label="Stroke color"
+                            value={getStrokeColor(props.selectedElements)}
+                            onChange={updateStrokeColor}
+                        />
+                        <TextField 
+                            fullWidth variant="outlined" size="small" margin="dense"
+                            label="Stroke width"
+                            InputProps={{
+                                endAdornment: <InputAdornment position="end">mm</InputAdornment>  
+                            }}
+                            value={strokeWidth}
+                            onChange={(e) => updateStrokeWidth(e.target.value)}
+                            sx={{backgroundColor: "background.default"}}
+                        />
                     </AccordionDetails>
                 </Accordion>
                 <Accordion disableGutters={true}  defaultExpanded={true}>
@@ -279,7 +363,22 @@ const ElementsPropertiesTab = (props: DiagramPropertiesPanelProps) => {
                         <Typography>Text</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        
+                    <ColorInput 
+                            fullWidth variant="outlined" size="small" margin="dense"
+                            label="Text color"
+                            value={getTextColor(props.selectedElements)}
+                            onChange={updateTextColor}
+                        />
+                        <TextField 
+                            fullWidth variant="outlined" size="small" margin="dense"
+                            label="Font size"
+                            InputProps={{
+                                endAdornment: <InputAdornment position="end">mm</InputAdornment>  
+                            }}
+                            value={fontSize}
+                            onChange={(e) => updateFontSize(e.target.value)}
+                            sx={{backgroundColor: "background.default"}}
+                        />
                     </AccordionDetails>
                 </Accordion>
             </>
