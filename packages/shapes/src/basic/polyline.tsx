d42b93eb20d85edda3f654c2430bb76d03546a08
YMSpektor <spektor.ym@gmail.com>
2023-08-21 10:16:09 +0300

    Add line caps for Polyline element

--- packages/shapes/src/basic/polyline.tsx
+++ packages/shapes/src/basic/polyline.tsx
@@ -2,12 +2,14 @@
 
 import { DiagramElement } from "@carnelian-diagram/core";
 import { withInteractivePolyline } from "@carnelian-diagram/interaction";
+import { radToDeg } from "@carnelian-diagram/interaction/geometry";
 import { PolylineBaseProps } from "..";
+import { LineCap } from "../line-caps";
 
 export interface PolylineProps extends PolylineBaseProps {}
 
 export const Polyline: DiagramElement<PolylineProps> = function(props) {
-    const { points, onChange, ...rest } = props;
+    const { points, onChange, startLineCap, endLineCap, ...rest } = props;
     const polylineProps = {
         ...rest,
         style: {
@@ -17,7 +19,27 @@ export const Polyline: DiagramElement<PolylineProps> = function(props) {
     }
 
     return (
-        <polyline points={points.map(p => `${p.x},${p.y}`).join(" ")} {...polylineProps} />
+        <>
+            <polyline points={points.map(p => `${p.x},${p.y}`).join(" ")} {...polylineProps} />
+            { startLineCap && points.length >= 2 &&
+                <LineCap 
+                    kind={startLineCap.kind} 
+                    size={startLineCap.size} 
+                    x={props.points[0].x} 
+                    y={props.points[0].y} 
+                    rotation={radToDeg(Math.atan2(props.points[0].y - props.points[1].y, props.points[0].x - props.points[1].x))} 
+                    style={polylineProps.style} 
+                /> }
+            { endLineCap && points.length >= 2 &&
+                <LineCap 
+                    kind={endLineCap.kind} 
+                    size={endLineCap.size} 
+                    x={props.points[props.points.length - 1].x} 
+                    y={props.points[props.points.length - 1].y} 
+                    rotation={radToDeg(Math.atan2(props.points[props.points.length - 1].y - props.points[props.points.length - 2].y, props.points[props.points.length - 1].x - props.points[props.points.length - 2].x))} 
+                    style={polylineProps.style} 
+                /> }
+        </>
     );
 };
 
